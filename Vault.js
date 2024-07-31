d1eb5f959aff4f20ec44fc744c9e5fcfe00c98af
William Sheere <sheerebilly@yahoo.com>
2024-07-31 15:29:50 -0400

    Vault Workshop

---
+++ Vault.js
@@ -0,0 +1,16 @@
+// Inputting lock combination
+// Create a varible that = 10 using addition
+// Create a variable that = 39 using subtraction
+// Create a variable that = 40 using multiplication
+
+//Step 1 Create message
+const lock =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(lock);
+alert(lock);
+// Step 2 create variables.
+const lock1 = 5 + 5;
+const lock2 = 50 - 11;
+const lock3 = 20 * 2;
+const combo = `${lock1}, ${lock2}, ${lock3}`;
+alert(combo);
